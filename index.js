c0488f6de425ce0d91dfcfae768d9dae796537f7
johned <johned.matt@gmail.com>
2016-10-14 21:00:07 -0400

    initial set up

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = requrire('express');
+var server =  express();
+
+var port = 8080;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.listen(port, function(){
+console.log('Now listening on port', port);
+});
