c01297f37007b233b721073c3e76adb9090eb964
johned <johned.matt@gmail.com>
2016-10-17 18:13:09 -0400

    heroku prep

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var express = requrire('express');
-var server =  express();
-
-var port = 8080;
+var express = require('express');
+var server = express();
+var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
 
-server.get('/', function(request, response){
-  response.sendFile('public/html/index.html', {root: __dirname});
+server.get("/", function(request, response){
+  response.sendFile("public/html/index.html", {root: __dirname});
 });
 
 server.listen(port, function(){
-console.log('Now listening on port', port);
+  console.log("Now listening on port...", port);
 });
