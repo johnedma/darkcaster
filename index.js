a58dedd8fe8c26b7b48262ddc44ed58eba65ddb2
johned <johned.matt@gmail.com>
2016-10-18 18:30:38 -0400

    added longitude

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
+var axios = require('axios');
+var apiKey = require('./config.js').apiKey;
 
 var port = process.env.PORT || 8080;
 
@@ -11,6 +13,21 @@ server.use(cors());
 
 server.get("/", function(request, response){
   response.sendFile("public/html/index.html", {root: __dirname});
+});
+
+server.get('/forecast/:latitude,:longitude', function(request, response){
+  var url = 'https://api.darksky.net/forecast/'+ apiKey +'/'+ request.params.latitude +','+ request.params.longitude;
+  var timeoutConfig = {
+    timeout: 2000
+  };
+  // response.send(" Latitude: " + request.params.latitude + " Longitude: " + request.params.longitude);
+  axios.get(url)
+  .then(function(forecast){
+    response.send(forecast.data);
+  })
+  .catch(function(error){
+    response.send(error);
+  });
 });
 
 server.listen(port, function(){
