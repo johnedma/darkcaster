c6caf172707068c461256ca3acbd1296dab99f08
johned <johned.matt@gmail.com>
2016-10-18 20:04:43 -0400

    added auth middleware

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
+var authorize = require('./middleware/auth.js');
 
 var forecastRouter = require('./routers/forecast.router.js');
+
 
 var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
 server.use(logger);
 server.use(cors());
+server.use(authorize);
 
 server.get("/", function(request, response){
   response.sendFile("public/html/index.html", {root: __dirname});
---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+var mySecretPhrase = process.env.PASS || require('../config.js').secretPassphrase;
+function authorize(request, response, next){
+  // console.log(request.headers);
+  // next();
+  var secretPassphrase = request.headers.secret;
+  if(!secretPassphrase || secretPassphrase !== mySecretPhrase){
+    response.status(403).json({
+      msg: "You are not authorized",
+      secretPassphrase: "its a secret"
+    });
+  } else {
+    next();
+  }
+}
+
+module.exports = authorize;
